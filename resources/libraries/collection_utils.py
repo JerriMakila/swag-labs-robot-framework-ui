34de788d7b28b6fa987bc1cc19e68a963365301c
Jerri Mäkilä <jerri.makila@gmail.com>
2026-06-14 23:07:30 +0300

    Ok now they're more done

---
+++ resources/libraries/collection_utils.py
@@ -0,0 +1,8 @@
+from robot.api.deco import keyword
+
+@keyword
+def find_dict_by_key_value (data: list, key: str, value: str) -> dict:
+    match = next((item for item in data if str(item.get(key)) == str(value)), None)
+    if match is None:
+        raise ValueError(f"No match found for {key}='{value}'")
+    return match
